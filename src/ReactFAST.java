1837c0551f6b241059f40a496e4bd7ea173f5123
nadinedelia <nadine.delia@yahoo.com>
2020-09-07 18:29:30 +0200

    delay for countdown

--- src/ReactFAST.java
+++ src/ReactFAST.java
@@ -1,13 +1,17 @@
 
 public class ReactFAST {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		System.out.println("3");
+		Thread.sleep(1000);
 		System.out.println("2");
+		Thread.sleep(1000);
 		System.out.println("1");
+		Thread.sleep(1000);
 		
+		System.out.println("Go!");
 	}
 
 }
