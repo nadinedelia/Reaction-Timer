e201e445ba35ba6fa0679178669b7eaac321ec31
nadinedelia <nadine.delia@yahoo.com>
2020-09-07 18:24:46 +0200

    printing out countdown

--- src/ReactFAST.java
+++ src/ReactFAST.java
@@ -4,6 +4,10 @@ public class ReactFAST {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("3");
+		System.out.println("2");
+		System.out.println("1");
+		
 	}
 
 }
